ee19b3d7a35b82a7ec457ddc79ea21bcb361ee26
Aleksandar Rajic <aleks.rajic97@gmail.com>
2023-11-15 01:31:33 +0100

    first commit

---
+++ src/main/java/com/example/gitactionscicd/GitActionsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitactionscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionsCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitactionscicd/GitActionsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactionscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
